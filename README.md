c797a730f4b8080dcca201fbdd37ebbbefa731f8
dfialkov <dfialkov@mines.edu>
2020-05-26 12:12:36 -0600

    Completed template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # q-job-search-tracker
+
+Please provide info in your own keys.js file in the config directory if you want to host this for yourself 
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dfialkov:T7SD6OJcRMG7MHyf@cluster0-unygf.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+}
